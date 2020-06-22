f262c8cc5a4ae7aedaf67ed6e07222ed05827117
Sergey Litvinov <slitvinov@gmail.com>
2020-06-22 14:21:52 +0200

    add label

--- mnist/label.c
+++ mnist/label.c
@@ -3,15 +3,15 @@
 #include <stdint.h>
 #include <string.h>
 
-const char *me = "pgm";
-enum { MAGIC = 2051 };
+const char *me = "label";
+enum { MAGIC = 2049 };
 static size_t file_end(FILE *);
 static uint32_t to_int32b(uint32_t, uint32_t, uint32_t, uint32_t);
 
 static void
 usg(void)
 {
-    fprintf(stderr, "%s -s int -i file.idx3 > file.pgm\n", me);
+    fprintf(stderr, "%s -s int -i file.idx3\n", me);
     exit(2);
 }
 
@@ -27,12 +27,10 @@ main(int argc, char **argv)
     size_t pos;
     size_t size;
     uint32_t magic;
-    uint32_t n_columns;
-    uint32_t n_images;
-    uint32_t n_rows;
+    uint32_t n_items;
     uint32_t s;
     unsigned char bytes[4];
-    unsigned char *data;
+    unsigned char label;
 
     USED(argc);
     Iflag = 0;
@@ -93,57 +91,32 @@ main(int argc, char **argv)
         fprintf(stderr, "%s: fail to read '%s'\n", me, input_path);
         exit(2);
     }
-    n_images = to_int32b(bytes[0], bytes[1], bytes[2], bytes[3]);
-    if (fread(bytes, sizeof *bytes, 4, input_file) != 4) {
-        fprintf(stderr, "%s: fail to read '%s'\n", me, input_path);
-        exit(2);
-    }
-    n_rows = to_int32b(bytes[0], bytes[1], bytes[2], bytes[3]);
-    if (fread(bytes, sizeof *bytes, 4, input_file) != 4) {
-        fprintf(stderr, "%s: fail to read '%s'\n", me, input_path);
-        exit(2);
-    }
-    n_columns = to_int32b(bytes[0], bytes[1], bytes[2], bytes[3]);
+    n_items = to_int32b(bytes[0], bytes[1], bytes[2], bytes[3]);
     if ((pos = ftell(input_file)) == (size_t) -1) {
         fprintf(stderr, "%s:%d: ftell failed\n", __FILE__, __LINE__);
         exit(2);
     }
     size = file_end(input_file);
-    if (n_images * n_rows * n_columns != size - pos) {
-        fprintf(stderr, "%s: images, raws, colin: %d %d %d\n", me,
-                n_images, n_rows, n_columns);
+    if (n_items != size - pos) {
+        fprintf(stderr, "%s: items: %d\n", me, n_items);
         fprintf(stderr, "%s: size, pos: %ld %ld\n", me, size, pos);
         exit(2);
     }
-    if (s >= n_images) {
-        fprintf(stderr, "%s: n_images: %d >= %d\n", me, s, n_images);
+    if (s >= n_items) {
+        fprintf(stderr, "%s: n_items: %d >= %d\n", me, s, n_items);
         exit(2);
     }
-    if (fseek(input_file, s * n_rows * n_columns, SEEK_CUR) != 0) {
+    if (fseek(input_file, s, SEEK_CUR) != 0) {
         fprintf(stderr, "%s:%d: fseek failed\n", __FILE__, __LINE__);
         exit(2);
     }
-    if ((data = malloc(n_rows * n_columns * sizeof *data)) == NULL) {
-        fprintf(stderr, "%s: malloc failed\n", me);
-        exit(2);
-    }
-    if (fread(data, sizeof *bytes, n_rows * n_columns, input_file) !=
-        n_rows * n_columns) {
+    if (fread(&label, sizeof *bytes, 1, input_file) != 1) {
         fprintf(stderr, "%s: fail to read '%s'\n", me, input_path);
         exit(2);
     }
     if (Verbose)
-        fprintf(stderr, "%d %d %d\n", n_images, n_rows, n_columns);
-    fprintf(stdout, "P5\n");
-    fprintf(stdout, "%d %d\n", n_rows, n_columns);
-    fprintf(stdout, "%d\n", 0xFF);
-    if (fwrite(data, sizeof *data, n_rows * n_columns, stdout) !=
-        n_columns * n_rows) {
-        fprintf(stderr, "%s: fail to write to stdout\n", me);
-        exit(2);
-    }
-
-    free(data);
+        fprintf(stderr, "%d", n_items);
+    printf("%d\n", label);
     if (fclose(input_file) != 0) {
         fprintf(stderr, "%s: fail to close '%s'\n", me, input_path);
         exit(2);
