394ba0febce4b5ceb3dc7f406ece1a3903ae2eaa
Sergey Litvinov <slitvinov@gmail.com>
2020-06-22 15:04:28 +0200

    add labels

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -3,21 +3,43 @@
 import sys
 import numpy
 me = "mnist"
-path = "test.images.idx3"
-MAGIC = 2051
-with open(path) as file:
-    magic, = numpy.fromfile(file, dtype=">u4", count=1)
-    if magic != MAGIC:
-        sys.stderr.write("%s: wrong magic number %d\n" % (me, magic))
-        sys.exit(2)
-    n_images, n_rows, n_columns = numpy.fromfile(file, dtype=">u4", count=3)
-    a = numpy.fromfile(file, dtype = "u1")
-    if numpy.size(a) != n_images * n_rows * n_columns:
-        sys.stderr.write("%s: wrong file size\n" % me)
-        sys.exit(2)
-    a = a.reshape(n_images, n_rows, n_columns)
+images_path = "train.images.idx3"
+labels_path = "train.labels.idx1"
 
-s = a[1891]
+def images(path):
+    MAGIC = 2051
+    with open(path) as file:
+        magic, = numpy.fromfile(file, dtype=">u4", count=1)
+        if magic != MAGIC:
+            sys.stderr.write("%s: wrong magic number %d\n" % (me, magic))
+            sys.exit(2)
+        n_images, n_rows, n_columns = numpy.fromfile(file, dtype=">u4", count=3)
+        a = numpy.fromfile(file, dtype = "u1")
+        if numpy.size(a) != n_images * n_rows * n_columns:
+            sys.stderr.write("%s: wrong file size\n" % me)
+            sys.exit(2)
+    return a.reshape(n_images, n_rows, n_columns)
+
+def labels(path):
+    MAGIC = 2049
+    with open(path) as file:
+        magic, = numpy.fromfile(file, dtype=">u4", count=1)
+        if magic != MAGIC:
+            sys.stderr.write("%s: wrong magic number %d\n" % (me, magic))
+            sys.exit(2)
+        n_items, = numpy.fromfile(file, dtype=">u4", count=1)
+        a = numpy.fromfile(file, dtype = "u1")
+        if numpy.size(a) != n_items:
+            sys.stderr.write("%s: wrong file size\n" % me)
+            sys.exit(2)
+    return a
+
+a = images(images_path)
+l = labels(labels_path)
+print(l)
+sys.exit(0)
+
+s = a[56]
 with open("a.pgm", "w") as file:
     file.write("P5\n")
     file.write("%d %d\n" % s.shape)
