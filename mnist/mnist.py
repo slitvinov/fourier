e411d3a63b86266cbe6aa774c39131f13e93c897
Sergey Litvinov <slitvinov@gmail.com>
2020-06-22 16:18:34 +0200

    baseline logistic regression

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -3,9 +3,14 @@
 import numpy
 import scipy
 import sys
+from sklearn.linear_model import LogisticRegression
+
 me = "mnist"
-images_path = "train.images.idx3"
-labels_path = "train.labels.idx1"
+train_images = "train.images.idx3"
+train_labels = "train.labels.idx1"
+
+test_images = "test.images.idx3"
+test_labels = "test.labels.idx1"
 
 def images(path):
     MAGIC = 2051
@@ -20,7 +25,6 @@ def images(path):
             sys.stderr.write("%s: wrong file size\n" % me)
             sys.exit(2)
     return a.reshape(n_images, n_rows, n_columns)
-
 def labels(path):
     MAGIC = 2049
     with open(path) as file:
@@ -34,7 +38,6 @@ def labels(path):
             sys.stderr.write("%s: wrong file size\n" % me)
             sys.exit(2)
     return a
-
 def pgm(path, s):
     with open(path, "w") as file:
         file.write("P5\n")
@@ -42,16 +45,17 @@ def pgm(path, s):
         file.write("%d\n" % 0xFF)
         s.tofile(file)
 
-a = images(images_path)
-l = labels(labels_path)
-s = a[1]
+a = images(train_images)
+l = labels(train_labels)
 
-f = scipy.fft.fft2(s)
-n = a.shape[1]
-print(numpy.real(f[0,0]))
+reg = LogisticRegression(solver = 'lbfgs')
+b = a.reshape(a.shape[0], a.shape[1] * a.shape[2])
+reg.fit(b, l)
+print(reg.score(b, l))
 
-s0 = scipy.fft.ifft2(f)
-s0 = numpy.real(s0)
-s0 = numpy.asarray(s0, dtype = "u1")
-pgm("s0.pgm", s0)
-pgm("s.pgm", s)
+a0 = images(test_images)
+l0 = labels(test_labels)
+b0 = a0.reshape(a0.shape[0], a0.shape[1] * a0.shape[2])
+print(reg.score(b0, l0))
+
+
